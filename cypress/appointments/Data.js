7f97b0350a702ff200f83214bada5cd11016a4be
temelkovskabyanka <temelkovska.byanka@gmail.com>
2019-12-04 18:09:50 +0000

    making new app

---
+++ cypress/appointments/Data.js
@@ -0,0 +1,16 @@
+export default class Data{
+
+    get appointmentStartField(){
+        return ("12/12/2019 14:30pm");
+    }
+    get appointmentEndFiled(){
+        return("15:00 pm");
+    }
+    get appoinmentPersonField(){
+        return ("Zoey Chakovska");
+    }
+    get notesFiled(){
+        return("Meeting");
+    }
+
+}
